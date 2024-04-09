8eb27dc0b020d3fd13e1d9257dfe3e6c06536411
gabriel-ribeiro-099 <gabriel.ribeiro.099@ufrn.edu.br>
2024-04-09 20:18:29 -0300

    Versão 1

---
+++ src/main/java/br/ufrn/imd/Person.java
@@ -0,0 +1,15 @@
+package br.ufrn.imd;
+
+public class Person {
+
+    public String name;
+    public String hometown;
+    public Person(){}
+
+    public String hometown(){
+        return hometown;
+    }
+    public String name(){
+        return name;
+    }
+}
